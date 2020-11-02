1c550d8f27fdd72f761c917ce3b26a0c65821d68
Ard-Jan van Etten <ardjanve@gmail.com>
2020-11-02 14:18:26 +0100

    working product

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,34 @@ use chrono::prelude::*;
 
 const STRAAL: f32 = 100.0;
 const MIDDEN: Vector2 = Vector2::new(400.0, 400.0);
+const MILLISMIDDEN: Vector2 = Vector2::new(MIDDEN.x + STRAAL, MIDDEN.y - STRAAL - LETTERGROOTTE);
 const LETTERGROOTTE: f32 = 30.0;
 
 fn main () {
     let (mut rl, thread) = raylib::init()
         .size(800, 800)
-        .title("Hello, World")
+        .title("haha rust go 🅱️rrrr")
         .build();
      
     while !rl.window_should_close() {
         let now = Local::now();
 
-
         let mut d = rl.begin_drawing(&thread); 
         d.clear_background(Color::BLACK);
         d.draw_fps(10, 10);
 
-
         d.draw_line_v(MIDDEN, MIDDEN + tijdseenheid_naar_coordinaat(now.hour12().1, 12, STRAAL / 1.5), Color::WHITE);
         d.draw_line_v(MIDDEN, MIDDEN + tijdseenheid_naar_coordinaat(now.minute(), 60, STRAAL), Color::WHITE);
-        d.draw_line_v(MIDDEN, MIDDEN + tijdseenheid_naar_coordinaat(now.second() * 1000 + now.timestamp_subsec_millis(), 60 * 1000, STRAAL), Color::RED);
-
-
+        d.draw_line_v(MIDDEN, MIDDEN + tijdseenheid_naar_coordinaat(now.second(), 60, STRAAL), Color::RED);
+        d.draw_line_v(MILLISMIDDEN, MILLISMIDDEN + tijdseenheid_naar_coordinaat(now.timestamp_subsec_millis(), 1000, 20.0), Color::WHITE);
 
         for uur in 1..=12 {
-            let target_coordinaat = MIDDEN + tijdseenheid_naar_coordinaat(uur, 12, STRAAL + LETTERGROOTTE);
+            let target_coordinaat = MIDDEN + tijdseenheid_naar_coordinaat(uur, 12, STRAAL + LETTERGROOTTE + 10.0) - LETTERGROOTTE / 2.0;
             d.draw_text(&uur.to_string(), target_coordinaat.x as i32, target_coordinaat.y as i32, LETTERGROOTTE as i32, Color::WHITE);
         }
 
         for minuut in 1..=60 {
-            let target_coordinaat = MIDDEN + tijdseenheid_naar_coordinaat(minuut, 60, STRAAL);
+            let target_coordinaat = MIDDEN + tijdseenheid_naar_coordinaat(minuut, 60, STRAAL + 10.0);
             d.draw_circle(target_coordinaat.x as i32, target_coordinaat.y as i32, 4.0, Color::WHITE);
         }
     }
