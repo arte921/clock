e1b975cf8e31b1cdb2151a6fa065e2c3e2b88c04
Ard-Jan van Etten <ardjanve@gmail.com>
2020-11-03 09:27:26 +0100

    easier to modify

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,52 @@
 use raylib::prelude::*;
 use chrono::prelude::*;
 
-const STRAAL: f32 = 100.0;
-const MIDDEN: Vector2 = Vector2::new(200.0, 200.0);
-const MILLISMIDDEN: Vector2 = Vector2::new(MIDDEN.x + STRAAL, MIDDEN.y - STRAAL - LETTERGROOTTE);
-const LETTERGROOTTE: f32 = 30.0;
+struct Wijzer {
+    eenheid: u32,
+    lengte: f32,
+    kleur: Color
+}
+
+const STRAAL: f32 = 400.0;
+const MARGE: f32 = 0.05 * STRAAL;
+const MIDDEN: Vector2 = Vector2::new(STRAAL + MARGE, STRAAL + MARGE);
+
+// 86400000 per dag
+const WIJZERS: [Wijzer; 3] = [
+    Wijzer {eenheid: 43200000,  lengte: 0.4, kleur: Color::WHITE}, // uren
+    Wijzer {eenheid: 3600000,   lengte: 0.7, kleur: Color::BLUE }, // minuten
+    Wijzer {eenheid: 60000,     lengte: 0.8, kleur: Color::RED  }, // seconden
+];
 
 fn main () {
     let (mut rl, thread) = raylib::init()
-        .size(400, 400)
+        .size(2 * (MARGE + STRAAL) as i32, 2 * (MARGE + STRAAL) as i32)
         .title("haha rust go 🅱️rrrr")
         .build();
      
     while !rl.window_should_close() {
         let now = Local::now();
+        let milliseconden = now.num_seconds_from_midnight() * 1000 + now.timestamp_subsec_millis();
 
         let mut d = rl.begin_drawing(&thread); 
         d.clear_background(Color::BLACK);
-        d.draw_fps(10, 10);
+        d.draw_fps(0, 0);
 
-        // uren
-        teken_wijzer(now.hour12().1, 12, STRAAL / 1.5, Color::WHITE, MIDDEN, &mut d);
-        // minuten
-        teken_wijzer(now.minute(), 60, STRAAL, Color::WHITE, MIDDEN, &mut d);
-        // seconden
-        teken_wijzer(now.second(), 60, STRAAL, Color::RED, MIDDEN, &mut d);
-        // milliseconden
-        teken_wijzer(now.timestamp_subsec_millis(), 1000, 20.0, Color::WHITE, MILLISMIDDEN, &mut d);
+        for wijzer in WIJZERS.iter() {
+            teken_wijzer(milliseconden % wijzer.eenheid, wijzer.eenheid, wijzer.lengte * STRAAL, wijzer.kleur, MIDDEN, &mut d);
+        }
 
         // teken tekst en langere lijntjes voor uren
         for uur in 1..=12 {
-            let target_coordinaat_tekst = tijdseenheid_naar_coordinaat(uur, 12, STRAAL + LETTERGROOTTE * 1.5, MIDDEN) - LETTERGROOTTE / 3.0;
-            d.draw_text(&uur.to_string(), target_coordinaat_tekst.x as i32, target_coordinaat_tekst.y as i32, LETTERGROOTTE as i32, Color::WHITE);
-
-            let binnenste_coordinaat_lijn = tijdseenheid_naar_coordinaat(uur, 12, STRAAL + 10.0, MIDDEN);
-            let buitenste_coordinaat_lijn = tijdseenheid_naar_coordinaat(uur, 12, STRAAL + 30.0, MIDDEN);
+            let binnenste_coordinaat_lijn = tijdseenheid_naar_coordinaat(uur, 12, STRAAL, MIDDEN);
+            let buitenste_coordinaat_lijn = tijdseenheid_naar_coordinaat(uur, 12, STRAAL * 0.8, MIDDEN);
             d.draw_line_v(binnenste_coordinaat_lijn, buitenste_coordinaat_lijn, Color::WHITE);
-
         }
 
         // teken bolletjes voor minuten/seconden
         for i in 1..=60 {
-            // let target_coordinaat = tijdseenheid_naar_coordinaat(i, 60, STRAAL + 10.0, MIDDEN);
-            // d.draw_circle(target_coordinaat.x as i32, target_coordinaat.y as i32, 4.0, Color::WHITE);
-
-            let binnenste_coordinaat_lijn = tijdseenheid_naar_coordinaat(i, 60, STRAAL + 20.0, MIDDEN);
-            let buitenste_coordinaat_lijn = tijdseenheid_naar_coordinaat(i, 60, STRAAL + 30.0, MIDDEN);
+            let binnenste_coordinaat_lijn = tijdseenheid_naar_coordinaat(i, 60, STRAAL, MIDDEN);
+            let buitenste_coordinaat_lijn = tijdseenheid_naar_coordinaat(i, 60, STRAAL * 0.9, MIDDEN);
             d.draw_line_v(binnenste_coordinaat_lijn, buitenste_coordinaat_lijn, Color::WHITE);
         }
     }
